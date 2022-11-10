be8a408fa26cbe9ce8b401e329f658cfce89c1f7
33Irina <zyryanovai17@gmail.com>
2022-11-10 17:43:16 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int initialAccount = 100;
-        int add =1500;
+        int add = 1500;
 
         int bonus = 0; //add > 1000 ? add / 100 : 0;
         if (add > 1000) {
@@ -9,7 +9,7 @@ public class Main {
         }
 
         int finallAccount = initialAccount + add + bonus;
-                System.out.println("Итоговый счет: " + finallAccount);
+        System.out.println("Итоговый счет: " + finallAccount);
         System.out.println("Бонус: " + bonus);
     }
 }
